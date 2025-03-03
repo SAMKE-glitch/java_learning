e0d476d0e03853d3fd866661dd57f7689f2413cc
samke-glitch <sammythemwa@gmail.com>
2025-03-03 09:05:22 +0300

    calling methods

--- javaExample/Main.java
+++ javaExample/Main.java
@@ -1,18 +1,17 @@
 public class Main {
-    // Static method
-    static void myStaticMethod() {
-        System.out.println("Static methods can be called without creating objects");
+    // Create a fullthottle() method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
     }
-
-    // Public methods
-    public void myPublicMethod() {
-        System.out.println("Public methods must be called by creating objects");
+    // Create a speed() method and add a parameter
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
     }
     // main method
     public static void main(String[] args) {
-        myStaticMethod(); // call the sttic method
-        // myPublicMethod(); // This would compile an error
-        Main myObj = new Main();
-        myObj.myPublicMethod();
+        // calling the fullThrottle method
+        Main myCar = new Main();
+        myCar.fullThrottle();
+        myCar.speed(260);
     }
 }
