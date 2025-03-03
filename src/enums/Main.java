5bd910c7403b1a9d7e45b0fe186b7c37696f5434
samke-glitch <sammythemwa@gmail.com>
2025-03-03 14:30:05 +0300

    enums

---
+++ src/enums/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public static void main(String[] args) {
+        Level myLevel = Level.MEDIUM;
+        System.out.println(myLevel);
+    }
+}
