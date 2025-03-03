1cd3abe1d660f375479fde5cae295f4582df0906
samke-glitch <sammythemwa@gmail.com>
2025-03-03 08:36:12 +0300

    overding

---
+++ javaExample/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x;
+
+    public static void main(String[] args) {
+        Main myObj = new Main(); // Object
+        myObj.x = 40;
+        System.out.println(myObj.x);
+    }
+}
