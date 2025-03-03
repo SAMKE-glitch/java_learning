bd7031f89870cbd3586d2b3a8b1111e7baad0951
samke-glitch <sammythemwa@gmail.com>
2025-03-03 10:17:46 +0300

    new:abstract classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+// Code from filename: Main.java
+// abstract class
+abstract class Main {
+    public String fname = "John";
+    public int age = 24;
+    public abstract void study(); //abstract method
+}
+
+// Subclass (inherit from Main)
+class Student extends Main {
+    public int graduationYear = 2018;
+
+    @Override
+    public void study() {
+        // the body of the abstract method is provided here
+        System.out.println("Studying all day long");
     }
 }
