ef01a88208ca2473207f5822fd4e866eb2aaa440
samke-glitch <sammythemwa@gmail.com>
2025-03-03 16:47:49 +0300

    new: called the get Status method

--- src/enums/Main.java
+++ src/enums/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static  void main(String[] args) {
         Order order = new Order(1L, "John Doe");
         OrderService orderService = new OrderService();
+        order.getStatus();
 
         System.out.println("Before update: " + order);
 
