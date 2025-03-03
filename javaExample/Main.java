684e2c3ab5a6ea07dc9f6b63ac1fd21846885f42
samke-glitch <sammythemwa@gmail.com>
2025-03-03 09:26:50 +0300

    constructor with variables

--- javaExample/Main.java
+++ javaExample/Main.java
@@ -2,11 +2,11 @@ public class Main {
     int x;
 
     // class constructor for the Main class
-    public Main(){
-        x = 5;
+    public Main(int y){
+        x = y;
     }
     public static void main(String[] args) {
-        Main myObj = new Main();
+        Main myObj = new Main(5);
         System.out.println(myObj.x);
     }
 }
