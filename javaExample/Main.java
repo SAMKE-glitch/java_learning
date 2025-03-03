507f9dacbd236407d42409f5e261448efb14536f
samke-glitch <sammythemwa@gmail.com>
2025-03-03 09:24:28 +0300

    class constructor

--- javaExample/Main.java
+++ javaExample/Main.java
@@ -1,17 +1,12 @@
 public class Main {
-    // Create a fullthottle() method
-    public void fullThrottle() {
-        System.out.println("The car is going as fast as it can!");
+    int x;
+
+    // class constructor for the Main class
+    public Main(){
+        x = 5;
     }
-    // Create a speed() method and add a parameter
-    public void speed(int maxSpeed) {
-        System.out.println("Max speed is: " + maxSpeed);
-    }
-    // main method
     public static void main(String[] args) {
-        // calling the fullThrottle method
-        Main myCar = new Main();
-        myCar.fullThrottle();
-        myCar.speed(260);
+        Main myObj = new Main();
+        System.out.println(myObj.x);
     }
 }
