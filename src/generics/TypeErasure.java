e47537206c2c3873a3d0eafc7e2e9c29d672612b
samke-glitch <sammythemwa@gmail.com>
2025-03-20 08:38:48 +0300

    compile time

---
+++ src/generics/TypeErasure.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Java generics only exist at compile-time
+// And are removed when the code has compiled.
+// The process is called Type Erasure
+public class TypeErasure {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        String value = list.get(0);
+        System.out.println(value);
+
+    }
+}
+
