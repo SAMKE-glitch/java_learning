fb35fa6940a9181a656a897f6786eca24939024f
samke-glitch <sammythemwa@gmail.com>
2025-04-15 08:28:44 +0300

    enhanced looping

--- src/arrayList/Arraylist.java
+++ src/arrayList/Arraylist.java
@@ -14,6 +14,8 @@ public class Arraylist {
     }
 
     public void displayCars(){
+        for (int i = 0; i <cars.size(); i++)
+            System.out.println(cars.get(i));
         for (String car : cars){
             System.out.println(car);
         }
