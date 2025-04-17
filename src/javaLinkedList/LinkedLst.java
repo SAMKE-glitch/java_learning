42b12faace260f52557b639cc6d81918d6f811c4
samke-glitch <sammythemwa@gmail.com>
2025-04-17 08:33:02 +0300

    linkedList

---
+++ src/javaLinkedList/LinkedLst.java
@@ -0,0 +1,14 @@
+package javaLinkedList;
+import java.awt.*;
+import java.util.LinkedList;
+
+public class LinkedLst {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
