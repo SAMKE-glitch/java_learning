32bc0deda68c49b6ca36f00ae9e34f95a97a9e90
samke-glitch <sammythemwa@gmail.com>
2025-04-24 08:28:02 +0300

    adding item on hashset

---
+++ src/hashSet/Hashset.java
@@ -0,0 +1,13 @@
+package hashSet;
+
+import java.util.HashSet;
+
+public class Hashset {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<>();
+
+        cars.add("Volvo");
+        System.out.println(cars);
+
+    }
+}
