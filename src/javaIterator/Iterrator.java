3370797db98e3f9a33af236c8a4bec92d1050e46
samke-glitch <sammythemwa@gmail.com>
2025-04-25 08:43:20 +0300

    iterator objects

---
+++ src/javaIterator/Iterrator.java
@@ -0,0 +1,16 @@
+package javaIterator;
+
+import java.util.ArrayList;
+
+public class Iterrator {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("BMW");
+        cars.add("Lamborghini");
+        cars.add("Nissan");
+        cars.add("Pagani");
+        cars.add("Range Rover");
+        cars.add("Volkswagen");
+
+    }
+}
