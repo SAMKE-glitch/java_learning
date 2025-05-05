48231264df16fb71e97be7b97f6a33d2c85f508f
samke-glitch <samythemwa@gmail.com>
2025-05-05 12:34:32 +0300

    interfaces

--- src/interfaces/Animal.java
+++ src/interfaces/Animal.java
@@ -1,4 +1,5 @@
+// An interface, this is like an abstract class in python
 interface Animal {
     public void animalSound(); // interface method (does not have a body)
-    public void sleep(); // interface method (does ot have a body)
+    public void sleep(); // interface method (does not have a body)
 }
