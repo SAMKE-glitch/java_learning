27f180a7a2745e04430f814ef4cb1c55508b818a
samke-glitch <samythemwa@gmail.com>
2025-05-05 12:37:34 +0300

    teting

--- src/interfaces/Pig.java
+++ src/interfaces/Pig.java
@@ -3,6 +3,7 @@ class Pig implements Animal {
     public void animalSound() {
         // The body of animalSound() is provided here
         System.out.println("The pig says: wee wee");
+	System.out.println("Line two of the same pig saying: wee wee!");
     }
     public void sleep() {
         // The body of sleep() is provided here
