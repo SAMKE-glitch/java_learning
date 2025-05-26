f0eeb4369f41ef52703c403c068671249407e45f
samke-glitch <sammythemwa@gmail.com>
2025-05-26 16:08:25 +0300

    wrapper classes

---
+++ src/wrapperClasses/WrapperDemo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class WrapperDemo {
+	public static void main(String[] args) {
+		// Autoboxing: primitive to wrapper
+		int primitiveInt = 42;
+		Integer wrappedInt = primitiveInt;
+		System.out.println("Autoboxed Integr: " + wrappedInt);
+	}
+}
