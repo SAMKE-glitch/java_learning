74b016c5bcd97da83369f567b756af513c12b441
samke-glitch <sammythemwa@gmail.com>
2025-05-27 16:14:53 +0300

    exception example

---
+++ src/exceptionsTryCatch/ExceptionExample.java
@@ -0,0 +1,20 @@
+package exceptionsTryCatch;
+
+public class ExceptionExample {
+	public static void main(String[] args) {
+		int[] numbers = {10, 20, 30};
+
+		try {
+			// Thi line will throw ArrayIndexoutOfBoundsException
+			int result = numbers[5] / 2;
+			System.out.println("Result: " + result);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception Caught: " + e.getMessage());
+		} catch (ArithmeticException e) {
+			System.out.println("Arithmetic Exception: " + e.getMessage());
+		} finally {
+			System.out.println("This block always executes.");
+		}
+		System.out.println("The program continues...")
+	}
+}
