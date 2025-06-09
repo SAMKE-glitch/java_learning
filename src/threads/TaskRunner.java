8152654a66bfd4d28b9e8972a2a8958c10ff2e5a
samke-glitch <sammythemwa@gmail.com>
2025-06-09 17:09:28 +0300

    taskrunner

--- src/threads/TaskRunner.java
+++ src/threads/TaskRunner.java
@@ -7,4 +7,16 @@ public class TaskRunner implements Runnable {
 	public TaskRunner(int taskId) {
 		this.taskId = taskId;
 	}
+
+
+	public void run() {
+		System.out.println("Task " + taskId + " started on " + Thread.currentThread());
+		try {
+			// Simulate work
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		System.out.println("Task " + taskId + " completed on " + Thread.currentThread());
+	}
 }
