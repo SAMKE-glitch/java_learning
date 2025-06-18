67c2f9d518cc01284e0177e4cf9ade9bbb52e11f
samke-glitch <sammythemwa@gmail.com>
2025-06-18 16:57:06 +0300

    lambda class

--- src/lambda/Lambdan.java
+++ src/lambda/Lambdan.java
@@ -4,17 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
-public class Lambdan {
+public class LambdaProcessor {
     /**
      * LambdaProcessor demonstrates functional programming using Java lambdas.
      * It trasnformms a list of integers by applying a given lambda experession
      *
      */	
-    
-    public static void main(String[] args) {
-     i                *
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        // Using Lambda to double each number
-        numbers.forEach(n -> System.out.println(n * 2));
+    private final List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+
+    public void process(Funtion<Integer, Integer> transformer) {
+    	numbers.forEach(n -> System.out.println(transformer.apply(n)));
+    }
+
+    public static void main(String[] args)
+        LamdaProcessor processor = new LambdaProcessor();
+        
+        System.out.println("Doubled values:");
+	processor.process(n -> n*2);
+
+	System.out.println("Squared values:");
+	processor.process(n -> n * n);
+
+	System.out.println("Incremented values");
+	processor.process(n -> n+1)
+
+
     }
 }
