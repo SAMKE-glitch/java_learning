e0a9e30d995e12a92ef42b1997ba0ce6a45d8836
samke-glitch <sammythemwa@gmail.com>
2025-06-18 17:03:27 +0300

    doc strings

--- src/lambda/LambdaProcessor.java
+++ src/lambda/LambdaProcessor.java
@@ -4,17 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
+
+/*
+ * LambdaProcessor demonstrates functional programming using Java lambdas.
+ * It transforms a list of integers by applying a given lambda expression.
+ */
 public class LambdaProcessor {
-    /**
-     * LambdaProcessor demonstrates functional programming using Java lambdas.
-     * It trasnformms a list of integers by applying a given lambda experession
-     *
-     */	
+     
     private final List<Integer> numbers = Arrays.asList(1,2,3,4,5);
 
+    /*
+     * Applies a transformation to each number in the list and prints the result.
+     *
+     * @param transformer a lambda expression to transform the integer
+     */
     public void process(Funtion<Integer, Integer> transformer) {
     	numbers.forEach(n -> System.out.println(transformer.apply(n)));
     }
+
+    /*
+     * Application Entry point
+     */
 
     public static void main(String[] args)
         LamdaProcessor processor = new LambdaProcessor();
