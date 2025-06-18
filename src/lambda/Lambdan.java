dc7ff0830383320c0b22eb383cb3c58da37dee2f
samke-glitch <sammythemwa@gmail.com>
2025-06-18 16:47:41 +0300

    lambdas

--- src/lambda/Lambdan.java
+++ src/lambda/Lambdan.java
@@ -2,9 +2,17 @@ package lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class Lambdan {
+    /**
+     * LambdaProcessor demonstrates functional programming using Java lambdas.
+     * It trasnformms a list of integers by applying a given lambda experession
+     *
+     */	
+    
     public static void main(String[] args) {
+     i                *
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         // Using Lambda to double each number
         numbers.forEach(n -> System.out.println(n * 2));
