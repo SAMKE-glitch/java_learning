08ef9d3c4a04c586037ff679d1ab29cec29da753
samke-glitch <sammythemwa@gmail.com>
2025-07-01 17:45:21 +0300

    Add: CountWords example to javahowtos package

---
+++ src/javahowtos/CountWords.java
@@ -0,0 +1,14 @@
+package javahowtos;
+
+
+public class CountWords {
+    public static void main(String[] args) {
+        String words = "One Two Three Four";
+
+	// Splitthe string by whitespace and count the resulting parts
+	int countWords = words.split("\\s+").length;
+
+	System.out.println("Input: " + words);
+	System.out.println(Word Count: ) countWords;
+    }
+}
