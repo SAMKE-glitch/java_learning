0c33857704807292718540f50836906ef140719b
samke-glitch <sammythemwa@gmail.com>
2025-07-09 16:45:52 +0300

    revserString

---
+++ src/javahowtos/ReverseString.java
@@ -0,0 +1,20 @@
+package javahowtos;
+
+
+public class ReverseString {
+    public static String reverse(String input) {
+        String reversed = "";
+	for (int i = 0; i < input.length(); i++) {
+	    reversed = input.charAt(i) = reversed;
+	}
+	return reversed;
+    }
+
+    public static void main(String[] args) {
+        String original = "Hello";
+	String result = reverse(original);
+
+	System.out.println("Original string: " + original);
+	System.out.println("Reversed string: "+ result)
+    }
+}
