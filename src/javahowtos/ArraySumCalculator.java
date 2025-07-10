4b18a752b74fa784468b10e690fb7e509e1c8246
samke-glitch <sammythemwa@gmail.com>
2025-07-10 16:36:52 +0300

    sum of elements in an array

---
+++ src/javahowtos/ArraySumCalculator.java
@@ -0,0 +1,22 @@
+package javahowtos
+
+public class ArraySumCalculator {
+    public static int calculateSum(int[] array) {
+        int sum = 0;
+
+	// Iterate over the array and accumulate the sum
+	for (int i = 0; i < array.length; i++) {
+	    sum += array[i]
+	}
+
+	return sum
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1, 5, 10, 25};
+
+	int result = calculateSum(myArray);
+
+	System.out.println("The sum is: "+ result);
+    }
+}
