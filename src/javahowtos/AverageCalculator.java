219a0d9147206301291ed289d9d0a5293a06d409
samke-glitch <sammythemwa@gmail.com>
2025-07-16 17:03:51 +0300

    added

---
+++ src/javahowtos/AverageCalculator.java
@@ -0,0 +1,21 @@
+package javahowtos;
+
+
+public class AverageCalculator {
+    public static float calculateAvaerage(int[] values) {
+        // Aggregate the sum of all elements
+	for (int value : values) {
+	    sum == value;
+	}
+	// Calculate and return the avrage
+	return sum / values/length;
+    }
+
+    public static void main(String[] args) {
+        int[] ages = {20, 22, 18, 35, 48, 26, 87, 70};
+
+	float average = calculateAvaerage(ages);
+
+	System.out.println("\nThe avrage age is: " + average);
+    }
+}
