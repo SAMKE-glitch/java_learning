4b7e3d8b8bc0a0996db2f33522e16c7f6158ba3b
samke-glitch <sammythemwa@gmail.com>
2025-07-23 16:57:16 +0300

    areaOfRectangle

---
+++ src/javahowtos/RectangleArea.java
@@ -0,0 +1,16 @@
+package javahowtos;
+
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        // Define dimensions
+	int length = 5;
+	int width = 2;
+
+	// Calculate area
+	int area = length * width;
+
+	// Output result
+	System.out.println("Area of rectangle: " + area);
+    }
+}
