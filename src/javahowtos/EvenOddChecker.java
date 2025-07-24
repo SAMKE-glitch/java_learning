7f253e62a11c10d8c13186c9182e25ec6c47b827
samke-glitch <sammythemwa@gmail.com>
2025-07-24 16:40:43 +0300

    even or odd

---
+++ src/javahowtos/EvenOddChecker.java
@@ -0,0 +1,14 @@
+package javahowtos;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = 5;
+
+	// Check if the number is even or odd
+	if (number % 2 == 0) {
+	    System.out.println(number + " is even.");
+	} else {
+	    System.out.println(number + " is odd.");
+	}
+    }
+}
