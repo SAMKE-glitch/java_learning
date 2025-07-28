7fafacde0c2eda3b6d5d19b8174313c14d52bb4e
samke-glitch <sammythemwa@gmail.com>
2025-07-28 16:49:45 +0300

    sqrt

--- src/javahowtos/SquareRootCalculator.java
+++ src/javahowtos/SquareRootCalculator.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 
 public class SquareRootCalculator {
     public static void main(String[] args) {
-        Scanner scanner = Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
 	System.out.println("Enter a number: ");
 	double number = scanner.nextDouble();
 
 	if (number < 0 ) {
-	    System.out.println("Error: Cannot compute square root of a negative number.")'
+	    System.out.println("Error: Cannot compute square root of a negative number.");
 	} else {
 	    double result = Math.sqrt(number);
 	    System.out.println("The square root of " + number + " is " + result);
 	}
 
-	Scanner.close();
+	scanner.close();
     }
 }
